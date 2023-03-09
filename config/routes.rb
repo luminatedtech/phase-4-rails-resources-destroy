ce3395ebb64ff8633c073b00a43e8523a48e810e
Allen Kim <avalmercafe02@gmail.com>
2023-03-09 02:23:09 -0500

    commit tree 5736f9628da5998d50a5625c0e0b15dffc4d71de

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
-  resources :birds, only: [:index, :show, :create, :update]
+  resources :birds
   patch "/birds/:id/like", to: "birds#increment_likes"
+  def destroy
+    bird = Bind.find_by(id: params[:id])
+    if bird 
+      bird.destroy
+      head :no_content
+    else
+      render json: {error: "Bird no found"}, status: :not_found
+    end 
+  end 
 end
